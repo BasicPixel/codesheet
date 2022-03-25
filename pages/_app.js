d9a32a3b5f17f1bec5bcd9d9b74dab09840a3918
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-03-25 14:38:47 +0300

    Add Chakra, favicon, meta tags, navbar and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,62 @@
-import '../styles/globals.css'
+import { ChakraProvider, Container } from "@chakra-ui/react";
+
+import { extendTheme } from "@chakra-ui/react";
+
+import "../styles/globals.css";
+
+import "@fontsource/fira-code";
+import "@fontsource/source-sans-pro";
+
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Source Sans Pro, sans-serif",
+    body: "Source Sans Pro, sans-serif",
+    mono: "Fira Code, monospace",
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Head>
+        <title>codesheet</title>
+        <meta
+          name="description"
+          content="a collection of cheatsheets for programming languages,
+          libraries, dev tools and more."
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <Container maxW={"full"} p={0}>
+        <Navbar />
+        <Container py={"4"}>
+          <Component {...pageProps} />
+        </Container>
+        <Footer />
+      </Container>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
